da6ea10a684dc6eea4ba907fdb85e39f890b848b
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2018-02-03 20:15:14 +0000

    Removido o listener para mensagem, já que ele é o genérico

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const TOKEN = process.env.TOKEN;
 const WEBHOOK_URL = process.env.WEBHOOK_URL || 'https://<app_name>.herokuapp.com:443';
+const CHATID = process.env.CHATID;
 
 const bot = new TelegramBot(TOKEN, {
   webHook: {
@@ -12,32 +13,22 @@ const bot = new TelegramBot(TOKEN, {
 bot.setWebHook(`${WEBHOOK_URL}/bot${TOKEN}`);
 
 // Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
+bot.onText(/\/echo (.+)/, (msg, match) => bot.sendMessage(msg.chat.id, match[1]));
 
 // Listen for 'photos'.
 bot.on('photo', (msg) => {
-  const chatId = process.env.CHATID; // Group ID;
   if (msg.chat.id === msg.from.id) { // Only forward messages from real people
       // Send the photo to the group
-      bot.sendPhoto(chatId, msg.photo[msg.photo.length-1].file_id);
+      bot.sendPhoto(CHATID, msg.photo[msg.photo.length-1].file_id);
   }
 });
 
 // Listen for 'videos'.
 bot.on('video', (msg) => {
-  const chatId = process.env.CHATID; // Group ID;
   if (msg.chat.id === msg.from.id) { // Only forward messages from real people
-    bot.sendVideo(chatId, msg.video.file_id);
+    bot.sendVideo(CHATID, msg.video.file_id);
   }
 });
 
 // Handling the error;
 bot.on('error', e => console.error(e.stack));
-
-// Echoing back the message
-bot.on('message', m => bot.sendMessage(m.chat.id, `Sua mensagem foi: ${m.text}`));
